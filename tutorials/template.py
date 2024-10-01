d52709f11b08941e679aad426654f0ff990a43c8
Moe Ariyoshi <moe.ariyoshi517@gmail.com>
2024-10-01 15:46:28 -0400

    add template file

---
+++ tutorials/template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Template File
+"""
+
+# import stuff
+
+# MAX_VALUE = 100
+
+# def add(x, y):
+#     """Return the sum of x and y."""
+#     return x + y
+
+# def subtract(x, y):
+#     """Return the difference of x and y."""
+#     return x - y
+
+# class Calculator:
+#     def __init__(self):
+#         pass
+
+# if __name__ == "__main__":
+#     result = add(5, 3)
+#     print(f"Result: {result}")
